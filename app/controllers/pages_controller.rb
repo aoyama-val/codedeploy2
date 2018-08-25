423f2fe51b886b6881cfa16600589b2afa5cda1b
Aoyama, Shotaro <aoyama@val.co.jp>
2018-08-26 00:08:22 +0900

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def root
-    render plain: "app10 こんにちは! #{Time.now.iso8601} hostname = #{`hostname`}"
+    render plain: "app11 こんにちは! #{Time.now.iso8601} hostname = #{`hostname`}"
   end
 end
