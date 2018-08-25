d003bcf80c9b067841035af21dfc2499469da8b8
Aoyama, Shotaro <aoyama@val.co.jp>
2018-08-25 22:15:32 +0900

    fix

--- deploy/start_app.sh
+++ deploy/start_app.sh
@@ -9,6 +9,6 @@ export RAILS_ENV=production
 export SECRET_KEY_BASE='1lkjljljklwrjglwejgjsdfn4ntk2n34jn4dnf34'
 
 cd /service/codedeploy2
-bin/bundle install -j4 --without development test
+bin/bundle install --path ./vendor -j4 --without development test
 bin/rails assets:precompile
 bin/rails server
